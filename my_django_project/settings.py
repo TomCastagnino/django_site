64195fba993564a1432319e62a768cde30627012
Tom Castagnino <tcastagnino@gmail.com>
2020-06-22 16:57:08 -0700

    further modifications of env vars in settings.py

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+if not config['DEBUG']:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
